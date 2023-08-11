f930b8eac343b7add4171beec59a4cbb90214cb2
GreenPlanet0204 <midasshong@gmail.com>
2023-08-11 02:29:32 -0800

    fix

--- server/settings.py
+++ server/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = 'django-insecure-urok4-m)droye=s%1*$o&y*s9w2p&2@y4mfljs&c1ml8)rrhdu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = []
 
 # Application definition
 
