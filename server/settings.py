171dc350a9d3906b88684f5ec2d007cec4624a5e
GreenPlanet0204 <midasshong@gmail.com>
2023-08-11 01:49:55 -0800

    add host

--- server/settings.py
+++ server/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = 'django-insecure-urok4-m)droye=s%1*$o&y*s9w2p&2@y4mfljs&c1ml8)rrhdu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'https://www.globalfirefly.com/',
+    'http://localhost:3000'
+]
 
 
 # Application definition
