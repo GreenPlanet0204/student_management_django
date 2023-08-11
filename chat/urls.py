d1fe79270ec9c5638c6f1c093748ebd51f35b2d9
GreenPlanet0204 <midasshong@gmail.com>
2023-08-11 02:17:38 -0800

    add fix

--- chat/urls.py
+++ chat/urls.py
@@ -4,7 +4,7 @@ from chat.views import ChatRoomView, MessagesView
 urlpatterns = [
 	path('chats/', ChatRoomView.as_view(), name='chatRoom'),
     path('chat/<str:roomId>/', ChatRoomView.as_view(), name='chatRoomRemove'),
-	path('chats/<str:roomId>/messages', MessagesView.as_view(), name='messageList'),
-	path('users/<int:userId>/chats', ChatRoomView.as_view(), name='chatRoomList'),
+	path('chats/<str:roomId>/messages/', MessagesView.as_view(), name='messageList'),
+	path('users/<int:userId>/chats/', ChatRoomView.as_view(), name='chatRoomList'),
 ]
 
