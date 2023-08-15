fb7ebdc3fd20e5a2d009863ee03606a50e64b17b
GreenPlanet0204 <midasshong@gmail.com>
2023-08-15 07:16:15 -0800

    'fix'

--- chat/views.py
+++ chat/views.py
@@ -5,35 +5,36 @@ from rest_framework.generics import ListAPIView
 from rest_framework.pagination import LimitOffsetPagination
 from chat.serializers import ChatRoomSerializer, ChatMessageSerializer
 from chat.models import ChatRoom, ChatMessage
+from api.models import CustomUser
+
 
 class ChatRoomView(APIView):
-	def get(self, request, userId):
-		chatRooms = ChatRoom.objects.filter(member=userId)
-		serializer = ChatRoomSerializer(
-			chatRooms, many=True, context={"request": request}
-		)
-		return Response(serializer.data, status=status.HTTP_200_OK)
+    def get(self, request, userId):
+        user = CustomUser.objects.get(id=userId)
+        chatRooms = user.chatroom_set.all()
+        serializer = ChatRoomSerializer(
+            chatRooms, many=True, context={"request": request}
+        )
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-	def post(self, request):
-		serializer = ChatRoomSerializer(
-			data=request.data, context={"request": request}
-		)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status=status.HTTP_200_OK)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-	
-	def delete(self, request):
-		if request.query_params.get("roomId", None) is not None:
-			chatRoom = ChatRoom.objects.get(roomId=request.query_params.get("roomId"))
-			chatRoom.delete()
-			return Response({"status": "success"}, status=status.HTTP_200_OK)
+    def post(self, request):
+        serializer = ChatRoomSerializer(data=request.data, context={"request": request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request):
+        if request.query_params.get("roomId", None) is not None:
+            chatRoom = ChatRoom.objects.get(roomId=request.query_params.get("roomId"))
+            chatRoom.delete()
+            return Response({"status": "success"}, status=status.HTTP_200_OK)
+
 
 class MessagesView(ListAPIView):
-	serializer_class = ChatMessageSerializer
-	pagination_class = LimitOffsetPagination
+    serializer_class = ChatMessageSerializer
+    pagination_class = LimitOffsetPagination
 
-	def get_queryset(self):
-		roomId = self.kwargs['roomId']
-		return ChatMessage.objects.\
-			filter(chat__roomId=roomId).order_by('-timestamp')
+    def get_queryset(self):
+        roomId = self.kwargs["roomId"]
+        return ChatMessage.objects.filter(chat__roomId=roomId).order_by("-timestamp")
