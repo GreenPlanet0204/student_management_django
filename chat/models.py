7ad42110f2f780d0cb164e49d145bceac5d0ca38
GreenPlanet0204 <midasshong@gmail.com>
2023-08-16 04:21:30 -0800

    'fix'

--- chat/models.py
+++ chat/models.py
@@ -2,19 +2,21 @@ from django.db import models
 from shortuuidfield import ShortUUIDField
 from api.models import CustomUser
 
+
 # Create your models here.
 class ChatRoom(models.Model):
     roomId = ShortUUIDField()
-    type = models.CharField(max_length=10, default='DM')
+    type = models.CharField(max_length=10, default="DM")
     member = models.ManyToManyField(CustomUser)
     name = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
-        return self.roomId + '->' + str(self.name)
-    
+        return self.roomId + "->" + str(self.name)
+
+
 class ChatMessage(models.Model):
-    chat = models.ForeignKey(ChatRoom, on_delete=models.CASCADE, null=True)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True)
+    chat = models.ForeignKey(ChatRoom, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True)
     message = models.CharField(max_length=255)
     timestamp = models.DateTimeField(auto_now_add=True)
 
