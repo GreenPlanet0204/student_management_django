20e6ab25e6ea2866ef8f1e7f38136444e22510e1
GreenPlanet0204 <midasshong@gmail.com>
2023-08-16 05:16:57 -0800

    'fix;

--- chat/urls.py
+++ chat/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path("chats/", ChatRoomView.as_view(), name="chatRoom"),
     path("chats/<str:roomId>/messages/", MessagesView.as_view(), name="messageList"),
     path("users/<int:userId>/chats/", ChatRoomView.as_view(), name="chatRoomList"),
-    path("rooms/<str:roomId>/", ChatRoomView.as_view(), name="chatRoomRemove"),
 ]
