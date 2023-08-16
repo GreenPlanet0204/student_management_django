8312f5b485989e9b624b00894ad11234be098648
GreenPlanet0204 <midasshong@gmail.com>
2023-08-16 03:31:50 -0800

    'fix'

--- chat/urls.py
+++ chat/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from chat.views import ChatRoomView, MessagesView
 
 urlpatterns = [
-	path('chats/', ChatRoomView.as_view(), name='chatRoom'),
-	path('chats/<str:roomId>/messages/', MessagesView.as_view(), name='messageList'),
-	path('users/<int:userId>/chats/', ChatRoomView.as_view(), name='chatRoomList'),
+    path("chats/", ChatRoomView.as_view(), name="chatRoom"),
+    path("chats/<str:roomId>/messages/", MessagesView.as_view(), name="messageList"),
+    # path('users/<int:userId>/chats/', ChatRoomView.as_view(), name='chatRoomList'),
 ]
-
